42cfbdea2278df4d660c4c2c264139922a30c74a
liangtianfu <liangtianfu985@126.com>
2019-06-24 23:34:19 +0800

    首页-楼层静态实现

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -10,11 +10,10 @@
     <swiper :indicator-dots="true" :autoplay="true" interval="2000">
       <block v-for="(item, index) in sildeList" :key="index">
         <swiper-item>
-          <image :src="item" class="slide-image" mode="aspectFill"/>
+          <image :src="item" class="slide-image" mode="aspectFill">
         </swiper-item>
       </block>
     </swiper>
-
     <!-- 分类导航 -->
     <view class="cate-nav">
       <image
@@ -23,7 +22,38 @@
         mode="aspectFill"
         v-for="(item, index) in 4"
         :key="index"
-      />
+      >
+    </view>
+    <!-- 楼层 -->
+    <view class="floor">
+      <!-- 每一个楼层 -->
+      <view class="floor-item">
+        <view class="floor-title">
+          <image
+            src="https://api.zbztb.cn/pyg/icon_index_nav_2@2x.png"
+            class="slide-image"
+            mode="aspectFill"
+          >
+        </view>
+        <view class="floor-content">
+          <view class="content-left">
+            <image
+              src="https://api.zbztb.cn/pyg/icon_index_nav_2@2x.png"
+              class="slide-image"
+              mode="aspectFill"
+            >
+          </view>
+          <view class="content-right">
+            <image
+              src="https://api.zbztb.cn/pyg/icon_index_nav_2@2x.png"
+              class="slide-image"
+              mode="aspectFill"
+              v-for="(item, index) in 4"
+              :key="index"
+            >
+          </view>
+        </view>
+      </view>
     </view>
   </view>
 </template>
@@ -74,6 +104,40 @@
     image {
       width: 150rpx;
       height: 180rpx;
+    }
+  }
+
+  .floor-item {
+    .floor-title {
+      image {
+        width: 750rpx;
+        height: 90rpx;
+      }
+    }
+    .floor-content {
+      image {
+        border-radius: 4px;
+      }
+      display: flex;
+      .content-left {
+        display: flex;
+        align-items: center;
+        image {
+          width: 150rpx * $unit;
+          height: 250rpx * $unit;
+        }
+      }
+      .content-right {
+        flex: 1;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        align-content: space-between;
+        image {
+          width: 155rpx * $unit;
+          height: 125rpx * $unit;
+        }
+      }
     }
   }
 </style>
