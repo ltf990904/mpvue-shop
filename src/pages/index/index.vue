322df95cda0edbd87ae81e5b8de61adb5cf7270f
liangtianfu <liangtianfu985@126.com>
2019-06-24 23:20:37 +0800

    首页-导航数据

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,20 +1,31 @@
 <template>
-  <div>
-    <div class="input-wrapper">
-      <div class="search-box">
+  <view>
+    <view class="input-wrapper">
+      <view class="search-box">
         <!-- mpvue中可以使用原生小程序组件，下面使用icon组件 -->
         <icon type="search" size="16"/>搜索
-      </div>
-    </div>
+      </view>
+    </view>
     <!-- 轮播图 -->
     <swiper :indicator-dots="true" :autoplay="true" interval="2000">
       <block v-for="(item, index) in sildeList" :key="index">
         <swiper-item>
-          <image :src="item" class="slide-image" mode="aspectFill"></image>
+          <image :src="item" class="slide-image" mode="aspectFill"/>
         </swiper-item>
       </block>
     </swiper>
-  </div>
+
+    <!-- 分类导航 -->
+    <view class="cate-nav">
+      <image
+        src="https://api.zbztb.cn/pyg/icon_index_nav_4@2x.png"
+        class="slide-image"
+        mode="aspectFill"
+        v-for="(item, index) in 4"
+        :key="index"
+      />
+    </view>
+  </view>
 </template>
 
 <script>
@@ -54,5 +65,15 @@
   .slide-image {
     width: 750rpx;
     height: 230rpx * $unit;
+  }
+
+  .cate-nav {
+    display: flex;
+    justify-content: space-around;
+    padding: 15rpx 0;
+    image {
+      width: 150rpx;
+      height: 180rpx;
+    }
   }
 </style>
