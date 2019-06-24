b903def776ed6ec9451b3fa596dd6cddac0f3537
liangtianfu <liangtianfu985@126.com>
2019-06-24 22:33:04 +0800

    搜索框(未进行封装)

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    首页
+    <div class="input-wrapper">
+      <div class="search-box">
+        <!-- mpvue中可以使用原生小程序组件，下面使用icon组件 -->
+        <icon type="search" size="16" /> 搜索
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +15,24 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+@import '../../styles/val.scss';
+.input-wrapper {
+  // 文字统一用px，小于5的 单位统一用px
+  padding: 10rpx*$unit;
+  background-color: #ff2d4a;
+  .search-box {
+    background-color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 16px;
+    height: 60rpx;
+    border-radius: 4px;
+    color: #cecece;
+    icon {
+      margin-right: 4px;
+    }
+  }
+}
 </style>
