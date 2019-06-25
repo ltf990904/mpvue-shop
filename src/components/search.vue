aec86a52ea1a3d137091e1ac374ba2f365b8292f
liangtianfu <liangtianfu985@126.com>
2019-06-25 12:43:39 +0800

    搜索组件的封装

---
+++ src/components/search.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="input-wrapper">
+    <div class="search-box">
+      <!-- mpvue中可以使用原生小程序组件，下面使用icon组件 -->
+      <icon type="search" size="16"/>搜索
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+  .input-wrapper {
+    // 文字统一用px，小于5的 单位统一用px
+    padding: 20rpx;
+    background-color: #ff2d4a;
+    .search-box {
+      background-color: #fff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 16px;
+      height: 60rpx;
+      border-radius: 4px;
+      color: #cecece;
+      icon {
+        margin-right: 4px;
+      }
+    }
+  }
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
-    <div class="input-wrapper">
-      <div class="search-box">
-        <!-- mpvue中可以使用原生小程序组件，下面使用icon组件 -->
-        <icon type="search" size="16"/>搜索
-      </div>
-    </div>
+    <!-- 搜索框 -->
+    <Search/>
     <!-- 轮播图 -->
     <swiper :indicator-dots="true" :autoplay="true" interval="2000">
       <block v-for="(item, index) in sliderList" :key="index">
@@ -66,7 +62,11 @@
 
 <script>
   import request from "../../utils/request.js";
+  import Search from '../../components/search'
   export default {
+    components: {
+      Search
+    },
     data() {
       return {
         sliderList: [], // 轮播图数组
@@ -111,24 +111,7 @@
 
 <style lang="scss" scoped>
   @import "../../styles/val.scss";
-  .input-wrapper {
-    // 文字统一用px，小于5的 单位统一用px
-    padding: 10rpx * $unit;
-    background-color: #ff2d4a;
-    .search-box {
-      background-color: #fff;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 16px;
-      height: 60rpx;
-      border-radius: 4px;
-      color: #cecece;
-      icon {
-        margin-right: 4px;
-      }
-    }
-  }
+  
   .slide-image {
     width: 750rpx;
     height: 230rpx * $unit;
