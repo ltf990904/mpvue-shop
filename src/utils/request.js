09feecf4f935bbbe9c09635d8e28ad24354eaac1
liangtianfu <liangtianfu985@126.com>
2019-06-25 23:13:28 +0800

    添加正在加载动画

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,18 @@
 function request(url, method = 'GET', data = {}) {
   // 在这个函数中，应该调用wx.request方法发送请求，并将请求成功的结果给到promise对象中，目的是为了在其他页面调用这个函数时，能够直接使用.then()获取成功的数据
   return new Promise((resolve, reject) => {
+    // 请求数据前先弹出加载框
+    wx.showLoading({
+      title: '正在加载',
+      mask: true
+    })
     wx.request({
       url: url,
       method: method,
       data: data,
       success: (res) => {
+        // 数据加载成功之后隐藏加载框
+        wx.hideLoading()
         // 将成功的数据放进resolve函数中
         resolve(res)
       }
