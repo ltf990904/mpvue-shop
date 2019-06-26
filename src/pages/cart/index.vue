449b33c06f77d3d199752a6c826a791d0a27037c
liangtianfu <liangtianfu985@126.com>
2019-06-26 22:03:42 +0800

    购物车页面数据渲染

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -1,15 +1,275 @@
 <template>
   <div>
-    购物车
+    <!-- 空购物车 -->
+    <div class="empty-cart" v-if="cartListPage.length === 0">
+      <image src="/static/images/cart_empty@2x.png" mode="aspectFill"></image>
+      <div class="empty-cart-text">购物车还是空的</div>
+      <div class="shop-btn" @tap="goToIndex">去逛逛</div>
+    </div>
+
+    <div class="cart-body" v-else>
+      <div class="empty-address">
+        <div class="add-btn">添加地址</div>
+      </div>
+      <div class="address">
+        <div class="user-info">
+          <span>收货人：xxxx</span>
+          <span>13112345678</span>
+        </div>
+        <div class="user-address">收货地址：xxxxx</div>
+      </div>
+      <image class="cart-border" src="/static/images/cart_border@2x.png"></image>
+      <div class="shop-name">
+        <span class="iconfont iconshop"></span>
+        <span>优购生活馆</span>
+      </div>
+      <div class="cart-list">
+        <div class="cart-list-item" v-for="(item, index) in cartListPage" :key="index">
+          <div class="cart-goods-status">
+            <span class="iconfont iconcheckbox-marked-circ active"></span>
+          </div>
+          <div class="cart-goods-info">
+            <image :src="item.goods_small_logo" mode="aspectFill"></image>
+            <div class="goods-info-box">
+              <h3>{{item.goods_name}}</h3>
+              <div class="info-box-bottom">
+                <span class="goods-price">￥{{item.goods_price}}</span>
+                <div class="goods-count">
+                  <span class="count-add iconfont iconminus"></span>
+                  <span class="count">{{item.counts}}</span>
+                  <span class="count-minus iconfont iconplus"></span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- footer -->
+    <div class="footer">
+      <div class="footer-item item-left">
+        <span class="iconfont iconcheckbox-marked-circ active"></span>
+        <span>全选</span>
+      </div>
+      <div class="footer-item item-center">
+        <div>
+          合计:
+          <span class="total-price">23232.00</span>
+        </div>
+        <div class="express">包含运费</div>
+      </div>
+      <div class="footer-item item-right">计算(3)</div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    data() {
+      return {
+        cartListPage: []
+      };
+    },
+    onLoad() {
+      // 2.1 首先从本地读取购物车数据，根据该数据判断是否有数据，如果有就展示数据，如果没有就显示购物车为空
+      this.cartListPage = wx.getStorageSync("cartList") || [];
+    },
+    methods: {
+      // 跳转到首页
+      goToIndex() {
+        wx.switchTab({
+          url: "/pages/index/main"
+        });
+      }
+    }
+  };
 </script>
 
-<style>
+<style lang="scss" scoped>
+  // 空购物车样式
+  .empty-cart {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    image {
+      width: 180rpx;
+      height: 180rpx;
+    }
+    .empty-cart-text {
+      line-height: 3;
+      color: #999;
+    }
+    .shop-btn {
+      background: #ed601b;
+      border-radius: 4px;
+      padding: 20rpx 30rpx;
+      color: #fff;
+    }
+  }
 
+  .cart-body {
+    padding: 20rpx;
+    // 空地址
+    .empty-address {
+      height: 60px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .add-btn {
+        padding: 20rpx 30rpx;
+        background: #a7a6a7;
+        color: white;
+        border-radius: 4px;
+        font-size: 16px;
+      }
+    }
+    // 地址
+    .address {
+      .user-info,
+      .user-address {
+        height: 60rpx;
+        line-height: 60rpx;
+        font-size: 16px;
+        word-break: break-all;
+        display: -webkit-box;
+        -webkit-line-clamp: 1;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+      }
+      .user-info {
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+    // 图片边框
+    .cart-border {
+      height: 15rpx;
+      width: 750rpx;
+      border-bottom: 15rpx solid #eee;
+    }
+    // 商店名字
+    .shop-name {
+      font-size: 16px;
+      height: 60rpx;
+      line-height: 60rpx;
+      border-bottom: 1px solid #eee;
+      margin-bottom: 20rpx;
+    }
+    // 购物车商品项
+    .cart-list-item {
+      display: flex;
+      &:last-child {
+        padding-bottom: 120rpx;
+        & > .cart-goods-info {
+          border: none;
+        }
+      }
+      .cart-goods-status {
+        width: 60rpx;
+        align-self: center;
+      }
+      .cart-goods-info {
+        flex: 1;
+        display: flex;
+        padding-bottom: 20rpx;
+        border-bottom: 1px solid #eee;
+        image {
+          align-self: center;
+          width: 150rpx;
+          height: 150rpx;
+          flex-shrink: 0;
+          margin-right: 20rpx;
+        }
+        .goods-info-box {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          h3 {
+            margin-bottom: 20rpx;
+            word-break: break-all;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+            overflow: hidden;
+          }
+          .info-box-bottom {
+            display: flex;
+            justify-content: space-between;
+            height: 60rpx;
+            line-height: 60rpx;
+            .goods-price {
+              color: #ff2d4a;
+              font-size: 16px;
+            }
+            .count-add,
+            .count-minus {
+              display: inline-block;
+              text-align: center;
+              border: 1px solid #666666;
+              height: 60rpx;
+              width: 60rpx;
+              box-sizing: border-box;
+            }
+            .count {
+              font-size: 14px;
+              margin: 0 20rpx;
+              height: 60rpx;
+              width: 60rpx;
+            }
+          }
+        }
+      }
+    }
+  }
+  // footer
+  .footer {
+    height: 120rpx;
+    display: flex;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: #fff;
+    .footer-item {
+      flex: 1;
+      display: flex;
+      width: 250rpx;
+      font-size: 16px;
+      .total-price {
+        color: #ff2d4a;
+      }
+    }
+
+    .item-left {
+      align-items: center;
+      padding-left: 20rpx;
+      & > span + span {
+        margin-left: 20rpx;
+      }
+    }
+    .item-center {
+      flex-direction: column;
+      justify-content: center;
+      .express {
+        font-size: 14px;
+        color: #a6a6a6;
+      }
+    }
+    .item-right {
+      justify-content: center;
+      align-items: center;
+      background: #ff2d4a;
+      color: #fff;
+    }
+  }
+  .active {
+    color: #ff2d4a;
+  }
 </style>
